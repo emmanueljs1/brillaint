770f6c8b4bd873a0eeaf87ce0d495434df1b24a9
Emmanuel Suarez Acevedo <19786004+emmanueljs1@users.noreply.github.com>
2025-02-11 21:33:16 -0500

    generalize, add constant propagation

--- bin/main.ml
+++ bin/main.ml
@@ -4,16 +4,77 @@ open Lib.Df
 open Lib.Syntax
 open Lib.Util
 
-let string_of_dom (d: domain) : string =
-  StrSet.to_list d
-  |> String.concat ", "
-  |> Printf.sprintf "{ %s }"
+module Defn : Domain = struct
+  type domain = StrSet.t
+  
+  let domain_eq = StrSet.equal
+  
+  let init = StrSet.empty
+  
+  let merge outs =
+    IntMap.fold (fun _ -> StrSet.union) outs StrSet.empty
+  
+  let transfer d i =
+    match dest_of_ins i with
+    | Some (dst, _) -> StrSet.add dst d
+    | _ -> d
 
-let () =
-  let p = Yojson.Raw.from_channel stdin |> prog_of_json in
-  let cfgs = List.map (fun f -> (f.name, mk_cfg f.instrs)) p in
-  List.iter (fun (fn, cfg) ->
-    Printf.sprintf "%s:" fn |> print_endline;
+  let string_of_domain d =
+    if StrSet.is_empty d then
+      "∅"
+    else
+      StrSet.to_list d |> String.concat ", "
+end
+
+module Defined = Analysis(Defn)
+
+module Constants : Domain = struct
+  type domain = (lit option) StrMap.t
+
+  let domain_eq = StrMap.equal (=)
+
+  let init = StrMap.empty
+
+  let merge outs =
+    IntMap.fold (fun _ ->
+      StrMap.merge (fun _ v1 v2 ->
+        match (v1, v2) with
+        | v1, None -> v1
+        | None, v2 -> v2
+        | Some v1, Some v2 -> if v1 = v2 then Some v1 else Some None
+      )
+    ) outs StrMap.empty
+
+  let transfer d i =
+    match i with
+    | Const ((dst, _), lit) ->
+        StrMap.add dst (Some lit) d
+    | Assign ((dst, _), _) ->
+        StrMap.add dst None d
+    | _ -> d
+
+  let string_of_domain d =
+    if StrMap.is_empty d then
+      "∅"
+    else
+      StrMap.to_list d
+      |> List.map (fun (x, v_opt) ->
+          let v =
+            match v_opt with
+            | None -> "?"
+            | Some lit -> json_of_lit lit |> Yojson.Raw.to_string
+          in
+          Printf.sprintf "%s -> %s" x v
+      )
+      |> String.concat ", "
+end
+
+module CProp = Analysis(Constants)
+
+type analysis = { analyze : cfg -> unit }
+
+let mk_analysis (df: cfg -> 'a dataflow) (show: 'a -> string) : analysis =
+  { analyze = fun cfg ->
     let (ins, outs) = df cfg in
     IntMap.iter (fun b _ ->
       let lbl =
@@ -23,8 +84,21 @@ let () =
       in
       Printf.sprintf "  %s:" lbl |> print_endline;
       let inb = IntMap.find b ins in
-      string_of_dom inb |> Printf.sprintf "    in: %s" |> print_endline;
+      show inb |> Printf.sprintf "    in: %s" |> print_endline;
       let outb = IntMap.find b outs in
-      string_of_dom outb |> Printf.sprintf "    out: %s" |> print_endline
+      show outb |> Printf.sprintf "    out: %s" |> print_endline
     ) cfg.blocks
+  }
+
+let () =
+  let analysis =
+    match (try Sys.argv.(1) with Invalid_argument _ -> "const") with
+    | "const" -> mk_analysis CProp.df Constants.string_of_domain
+    | _ -> mk_analysis Defined.df Defn.string_of_domain
+  in
+
+  let p = Yojson.Raw.from_channel stdin |> prog_of_json in
+  let cfgs = List.map (fun f -> (f.name, mk_cfg f.instrs)) p in
+  List.iter (fun (fn, cfg) ->
+    Printf.sprintf "%s:" fn |> print_endline; analysis.analyze cfg
   ) cfgs
--- lib/df.ml
+++ lib/df.ml
@@ -2,48 +2,54 @@ open Cfg
 open Common
 open Syntax
 
-type domain = StrSet.t
+module type Domain = sig
+  type domain
 
-let domain_eq = StrSet.equal
+  val domain_eq : domain -> domain -> bool
 
-let init : domain = StrSet.empty
+  val init : domain
 
-let merge (outs: domain IntMap.t) : domain =
-  IntMap.fold (fun _ -> StrSet.union) outs StrSet.empty
+  val merge : domain IntMap.t -> domain
 
-let transfer (d: domain) (i: ins) : domain =
-  match dest_of_ins i with
-  | Some (dst, _) -> StrSet.add dst d
-  | _ -> d
+  val transfer : domain -> ins -> domain
 
-let transfer_block (b: block) (inb: domain) : domain =
-  List.fold_left transfer inb b
+  val string_of_domain : domain -> string
+end
 
-let df (cfg: cfg) : domain IntMap.t * domain IntMap.t =
-  let rec loop ins outs bs =
-    match IntSet.choose_opt bs with
-    | None -> ins, outs
-    | Some b ->
-      let outb = IntMap.find b outs in
-      let preds = predecessors b cfg in
-      let inb =
-        if b = cfg.entry then
-          init
-        else
-          IntMap.filter (fun b' _ -> IntMap.mem b' preds) outs |> merge
-      in
-      let outb' = transfer_block (IntMap.find b cfg.blocks) inb in
-      let bs' = IntSet.remove b bs in
-      let bs'' =
-        if domain_eq outb outb' then
-          bs'
-        else
-          IntSet.fold IntSet.add bs' (IntMap.find b cfg.successors)
-      in
-      loop (IntMap.add b inb ins) (IntMap.add b outb' outs) bs''
-  in
+type 'a dataflow = 'a IntMap.t * 'a IntMap.t
 
-  let ins = IntMap.empty in
-  let outs = IntMap.map (fun _ -> init) cfg.blocks in
-  let worklist = IntMap.fold (fun b _ -> IntSet.add b) cfg.blocks IntSet.empty in
-  loop ins outs worklist
+module Analysis (D : Domain) = struct
+  open D
+  
+  let transfer_block (b: block) (inb: domain) : domain =
+    List.fold_left transfer inb b
+  
+  let df (cfg: cfg) : domain dataflow =
+    let rec loop ins outs bs =
+      match IntSet.choose_opt bs with
+      | None -> ins, outs
+      | Some b ->
+        let outb = IntMap.find b outs in
+        let preds = predecessors b cfg in
+        let inb =
+          if b = cfg.entry then
+            init
+          else
+            IntMap.filter (fun b' _ -> IntMap.mem b' preds) outs |> merge
+        in
+        let outb' = transfer_block (IntMap.find b cfg.blocks) inb in
+        let bs' = IntSet.remove b bs in
+        let bs'' =
+          if domain_eq outb outb' then
+            bs'
+          else
+            IntSet.fold IntSet.add bs' (IntMap.find b cfg.successors)
+        in
+        loop (IntMap.add b inb ins) (IntMap.add b outb' outs) bs''
+    in
+  
+    let ins = IntMap.empty in
+    let outs = IntMap.map (fun _ -> init) cfg.blocks in
+    let worklist = IntMap.fold (fun b _ -> IntSet.add b) cfg.blocks IntSet.empty in
+    loop ins outs worklist
+end
